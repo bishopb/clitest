cd384ea96ecd222a5237392a526fc51d20688601
Bishop Bettini <bishop.bettini@gmail.com>
2025-06-24 23:03:11 -0400

    fix: be less sloppy with whitespace

--- helpers/basic.sh
+++ helpers/basic.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # A basic tool for testing stdout, stderr, and exit codes.
 if [ "$1" = "stdout" ]; then
-  echo "$2"
+  printf "$2"
   exit 0
 elif [ "$1" = "stderr" ]; then
-  echo "$2" >&2
+  printf "$2" >&2
   exit 1
 fi
